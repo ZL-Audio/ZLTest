5feca664194ece6b22e828d6609b9b9a23be9149
zsliu98 <zsliu98@gmail.com>
2024-05-05 21:49:49 -0400

    m

--- packaging/generator.py
+++ packaging/generator.py
@@ -9,6 +9,7 @@ def main():
     temp_dir = "./appletmp"
     subprocess.run(["mkdir", temp_dir])
 
+    project_name = os.getenv("PROJECT_NAME", "Pamplejuce")
     product_name = os.getenv("PRODUCT_NAME", "Pamplejuce Demo")
     version = os.getenv("VERSION", "0.0.0")
     bundle_id = os.getenv("BUNDLE_ID", "")
@@ -22,15 +23,15 @@ def main():
         ["vst3", "component", "lv2"],
         ["VST3", "Components", "LV2"]):
         if plugin_format + "_PATH" in os.environ:
-            plugin_path = build_dir + os.environ[plugin_format + "_PATH"]
+            plugin_path = build_dir + "/" + os.environ[plugin_format + "_PATH"]
             print(plugin_path)
             if os.path.exists(plugin_path):
                 print("Create {} package.".format(plugin_format))
                 subprocess.run("pkgbuild",
                                "--sign", cert,
-                               "--identifier", "{}.{}.pkg".format(bundle_id, extension),
+                               "--identifier", "{}.{}.{}.pkg".format(bundle_id, project_name, extension),
                                "--version", version,
-                               "--component", build_dir + plugin_path,
+                               "--component", build_dir + "/" + plugin_path,
                                "--install-location", "/Library/Audio/Plug-Ins/" + install_path,
                                "{}/{}.{}.pkg".format(temp_dir, product_name, extension))
 
