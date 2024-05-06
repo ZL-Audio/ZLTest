29e6d47f5ebb7dc18bdc8d0761c94e1e0a297f68
zsliu98 <zsliu98@gmail.com>
2024-05-06 10:30:22 -0400

    m

--- packaging/generator.py
+++ packaging/generator.py
@@ -95,11 +95,11 @@ def main():
         print("Attach icns")
         with open("tmpIcon.rsrc", "w") as outfile:
             subprocess.run(["echo", "read 'icns' (-16455) \"packaging/icon.icns\";"], stdout=outfile)
-        subprocess.run(["Rez", "-append", "tmpIcon.rsrc", "-o", "'{}.pkg'".format(artifacts_name)])
-        subprocess.run(["SetFile", "-a", "C", "'{}.pkg'".format(artifacts_name)])
+        subprocess.run(["Rez", "-append", "tmpIcon.rsrc", "-o", '"{}.pkg"'.format(artifacts_name)])
+        subprocess.run(["SetFile", "-a", "C", '"{}.pkg"'.format(artifacts_name)])
     print("")
     print("Zip package")
-    subprocess.run(["zip", "'{}.zip'".format(artifacts_name), "'{}.pkg'".format(artifacts_name)])
+    subprocess.run(["zip", '"{}.zip"'.format(artifacts_name), '"{}.pkg"'.format(artifacts_name)])
     return 0
 
 if __name__ == '__main__':
