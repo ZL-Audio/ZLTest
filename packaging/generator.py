345e832aa3a30b449b5dc3f67e271801d6204477
zsliu98 <zsliu98@gmail.com>
2024-05-06 09:49:16 -0400

    m

--- packaging/generator.py
+++ packaging/generator.py
@@ -93,8 +93,7 @@ def main():
     if os.path.exists("packaging/icon.icns"):
         print("")
         print("Attach icns")
-        with open("tmpIcon.rsrc", "w") as outfile:
-            subprocess.run(["echo", "read 'icns' (-16455) \"packaging/icon.icns\";"], stdout=outfile)
+        subprocess.call("""echo "read 'icns' (-16455) \"packaging/icon.icns\";" >> Icon.rsrc""")
         subprocess.run(["Rez", "-append", "tmpIcon.rsrc", "-o", artifacts_name + ".pkg"])
         subprocess.run(["SetFile", "-a", "C", artifacts_name + ".pkg"])
 
