067bf3a787cbf250bd94ed9eba4d94cb3f47cc30
zsliu98 <zsliu98@gmail.com>
2024-05-05 22:05:12 -0400

    m

--- packaging/generator.py
+++ packaging/generator.py
@@ -16,8 +16,7 @@ def main():
     build_dir = os.getenv("BUILD_DIR", "")
     cert = os.getenv("APPLE_INSTALLER_DEV", "")
 
-    print(product_name + " " + version)
-
+    print("Create packages")
     for plugin_format, extension, install_path in zip(
         ["VST3", "AU", "LV2"],
         ["vst3", "component", "lv2"],
@@ -25,13 +24,13 @@ def main():
         if plugin_format + "_PATH" in os.environ:
             plugin_path = build_dir + "/" + os.environ[plugin_format + "_PATH"]
             if os.path.exists(plugin_path):
-                print("Create {} package.".format(plugin_format))
+                print("\tcreate {} package".format(plugin_format))
                 subprocess.run([
                     "pkgbuild",
                     "--sign", cert,
                     "--identifier", "{}.{}.{}.pkg".format(bundle_id, project_name, extension),
                     "--version", version,
-                    "--component", build_dir + "/" + plugin_path,
+                    "--component", plugin_path,
                     "--install-location", "/Library/Audio/Plug-Ins/" + install_path,
                     "{}/{}.{}.pkg".format(temp_dir, product_name, extension)])
 
