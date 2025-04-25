96d7dc3fffc5ff0c44e412d2d9d9024b0346827c
zsliu98 <zsliu98@gmail.com>
2025-04-25 11:39:21 -0400

    m [skip ci]

--- packaging/github_msvc_setup.py
+++ packaging/github_msvc_setup.py
@@ -6,7 +6,7 @@ import subprocess
 
 def main():
     github_env = os.getenv("GITHUB_ENV", "")
-    for year in range(2022, 2099):
+    for year in range(2050, 2000, -1):
         msvc_path = 'C:/Program Files/Microsoft Visual Studio/{}/Enterprise/VC/Auxiliary/Build/vcvars64.bat'.format(year)
         msvc_path = os.path.abspath(msvc_path)
         if os.path.exists(msvc_path):
@@ -20,7 +20,7 @@ def main():
                 for line in result.stdout.splitlines():
                     if '=' in line:
                         print(line)
-                        f.write(line)
+                        f.write(line.encode())
             return
 
 if __name__ == '__main__':
