452edf9fdd65ef57354a567c38acd26481a99079
zsliu98 <zsliu98@gmail.com>
2025-09-09 22:41:53 -0400

    perf(over sample): improve performance

--- source/over_sample/over_sample_stage.hpp
+++ source/over_sample/over_sample_stage.hpp
@@ -11,8 +11,7 @@
 
 #include <span>
 #include <numbers>
-
-#include <juce_audio_basics/juce_audio_basics.h>
+#include <algorithm>
 
 namespace zldsp::oversample {
     template<typename FloatType>
@@ -36,23 +35,32 @@ namespace zldsp::oversample {
             latency_ = (up_coeff.size() + down_coeff.size() - 2) / 4;
         }
 
-        void prepare(const size_t num_channels, const size_t num_samples) {
+        void prepare(const size_t num_channels, const size_t max_num_samples) {
+            const size_t up_req_size = up_coeff_.size();
+            const size_t down_req_size = down_coeff_.size();
+
+            const size_t up_delay_size = up_req_size + max_num_samples + 1;
             up_delay_lines_.resize(num_channels);
-            for (auto &d : up_delay_lines_) {
-                d.resize(up_coeff_.size());
+            for (auto &d: up_delay_lines_) {
+                d.resize(up_delay_size);
             }
+
+            const size_t down_delay_size = down_req_size + max_num_samples + 1;
             down_delay_lines_.resize(num_channels);
-            for (auto &d : down_delay_lines_) {
-                d.resize(down_coeff_.size());
+            for (auto &d: down_delay_lines_) {
+                d.resize(down_delay_size);
             }
+
             down_center_delay_lines_.resize(num_channels);
-            for (auto &d : down_center_delay_lines_) {
+            for (auto &d: down_center_delay_lines_) {
                 d.resize(down_coeff_.size() / 2);
             }
+
             os_buffers_.resize(num_channels);
-            for (auto &buffer:os_buffers_) {
-                buffer.resize(num_samples << 1);
+            for (auto &buffer: os_buffers_) {
+                buffer.resize(max_num_samples << 1);
             }
+
             os_pointers_.resize(num_channels);
             for (size_t chan = 0; chan < num_channels; ++chan) {
                 os_pointers_[chan] = os_buffers_[chan].data();
@@ -61,8 +69,6 @@ namespace zldsp::oversample {
         }
 
         void reset() {
-            up_cursor_ = 0;
-            down_cursor_ = 0;
             down_center_pos_ = 0;
             for (auto &d: up_delay_lines_) {
                 std::fill(d.begin(), d.end(), FloatType(0));
@@ -75,86 +81,81 @@ namespace zldsp::oversample {
             }
         }
 
-        [[nodiscard]] size_t getLatency() const {
-            return latency_;
-        }
+        [[nodiscard]] size_t getLatency() const { return latency_; }
 
-        /**
-         * process samples up
-         * @param buffer input samples
-         * @param num_samples
-         */
-        template<bool UseSIMD = true>
+        template<bool use_simd = false>
         void upsample(std::span<FloatType *> buffer, const size_t num_samples) {
-            const auto symmetric_size = up_delay_lines_[0].size() >> 1;
-            const auto symmetric_shift = up_delay_lines_[0].size() - 1;
-            const auto memmove_size = (up_delay_lines_[0].size() - 1) * sizeof(FloatType);
+            const auto symmetric_size = up_coeff_.size() >> 1;
+            const auto symmetric_shift = up_coeff_.size() - 1;
+
             for (size_t chan = 0; chan < buffer.size(); ++chan) {
-                auto &delay_line{up_delay_lines_[chan]};
-                auto chan_data{buffer[chan]};
-                auto os_data{os_buffers_[chan].data()};
+                auto delay_line = up_delay_lines_[chan].data();
+                auto os_data = os_buffers_[chan].data();
+                auto chan_data = buffer[chan];
                 for (size_t i = 0; i < num_samples; ++i) {
-                    os_data[i << 1] = delay_line[up_coeff_center_pos_] * up_coeff_center_;
-                    std::memmove(delay_line.data(), delay_line.data() + 1, memmove_size);
-                    delay_line[delay_line.size() - 1] = chan_data[i];
+                    os_data[i << 1] = delay_line[up_coeff_center_pos_ + i] * up_coeff_center_;
+                    delay_line[up_coeff_.size() + i] = chan_data[i];
+
                     FloatType output{FloatType(0)};
+                    const auto shifted_delay_line = &delay_line[i + 1];
                     for (size_t k = 0; k < symmetric_size; ++k) {
-                        output += (delay_line[k] + delay_line[symmetric_shift - k]) * up_coeff_[k];
+                        output += (shifted_delay_line[k] + shifted_delay_line[symmetric_shift - k]) * up_coeff_[k];
                     }
                     os_data[(i << 1) + 1] = output;
                 }
             }
+
+            const auto memmove_size = up_coeff_.size() * sizeof(FloatType);
+            for (auto &delay_line: up_delay_lines_) {
+                std::memmove(delay_line.data(), delay_line.data() + num_samples, memmove_size);
+            }
         }
 
-        /**
-         * process samples down
-         * @tparam UseSIMD use SIMD dot-product, faster for large filters
-         * @param buffer output samples
-         * @param num_samples
-         */
-        template<bool UseSIMD = true>
+        template<bool use_simd = false>
         void downsample(std::span<FloatType *> buffer, const size_t num_samples) {
-            const auto symmetric_size = down_delay_lines_[0].size() >> 1;
-            const auto symmetric_shift = down_delay_lines_[0].size() - 1;
-            const auto memmove_size = (down_delay_lines_[0].size() - 1) * sizeof(FloatType);
-            size_t center_pos{0};
+            const auto symmetric_size = down_coeff_.size() >> 1;
+            const auto symmetric_shift = down_coeff_.size() - 1;
+
+            size_t center_pos{down_center_pos_};
             for (size_t chan = 0; chan < buffer.size(); ++chan) {
-                auto &delay_line{down_delay_lines_[chan]};
-                auto chan_data{buffer[chan]};
-                auto os_data{os_buffers_[chan].data()};
+                auto delay_line = down_delay_lines_[chan].data();
+                auto center_delay_line = down_center_delay_lines_[chan].data();
+                auto os_data = os_buffers_[chan].data();
+                auto chan_data = buffer[chan];
                 center_pos = down_center_pos_;
-                auto &center_delay_line {down_center_delay_lines_[chan]};
                 for (size_t i = 0; i < num_samples; ++i) {
                     FloatType output = center_delay_line[center_pos] * down_coeff_center_;
+                    const auto shifted_delay_line = &delay_line[i];
                     for (size_t k = 0; k < symmetric_size; ++k) {
-                        output += (delay_line[k] + delay_line[symmetric_shift - k]) * down_coeff_[k];
+                        output += (shifted_delay_line[k] + shifted_delay_line[symmetric_shift - k]) * down_coeff_[k];
                     }
                     chan_data[i] = output;
-                    std::memmove(delay_line.data(), delay_line.data() + 1, memmove_size);
-                    delay_line.back() = os_data[(i << 1) + 1];
-                    center_delay_line[center_pos] = os_data[(i << 1)];
-                    center_pos = center_pos == 0 ? center_delay_line.size() - 1 : center_pos - 1;
+
+                    delay_line[down_coeff_.size() + i] = os_data[(i << 1) + 1];
+                    center_delay_line[center_pos] = os_data[i << 1];
+
+                    center_pos = (center_pos == 0) ? down_center_delay_lines_[chan].size() - 1 : center_pos - 1;
                 }
             }
+
             down_center_pos_ = center_pos;
+
+            const auto memmove_size = down_coeff_.size() * sizeof(FloatType);
+            for (auto &delay_line: down_delay_lines_) {
+                std::memmove(delay_line.data(), delay_line.data() + num_samples, memmove_size);
+            }
         }
 
-        std::vector<std::vector<FloatType> > &getOSBuffer() {
-            return os_buffers_;
-        }
+        std::vector<std::vector<FloatType> > &getOSBuffer() { return os_buffers_; }
 
-        std::vector<FloatType *> &getOSPointer() {
-            return os_pointers_;
-        }
+        std::vector<FloatType *> &getOSPointer() { return os_pointers_; }
 
     private:
-        size_t up_cursor_{0};
         std::vector<FloatType> up_coeff_{};
         FloatType up_coeff_center_{FloatType(0)};
         size_t up_coeff_center_pos_{0};
         std::vector<std::vector<FloatType> > up_delay_lines_{};
 
-        size_t down_cursor_{0};
         std::vector<FloatType> down_coeff_{};
         FloatType down_coeff_center_{FloatType(0)};
         std::vector<std::vector<FloatType> > down_delay_lines_{};
